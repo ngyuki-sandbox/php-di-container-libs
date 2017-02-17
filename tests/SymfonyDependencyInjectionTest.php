31b5442e2490bef65e1f121b6ea0964ee6551355
ngyuki <ngyuki.jp@gmail.com>
2017-02-17 14:48:28 +0900

    Symfony のインタフェースでのオートワイヤリングと Synthetic なサービス

--- tests/SymfonyDependencyInjectionTest.php
+++ tests/SymfonyDependencyInjectionTest.php
@@ -33,6 +33,21 @@ class SymfonyDependencyInjectionTest extends \PHPUnit_Framework_TestCase
         var_dump($are->say()); // Are -> Ore
     }
 
+    /**
+     * コンストラクタのタイプヒントがインタフェースでも
+     * 該当するサービスが１つしか登録されていないならそれが注入される
+     */
+    function test_AutoWiring_by_interface()
+    {
+        $container = new ContainerBuilder();
+        $container->register('ore', Ore::class);
+        $container->register('sore', Sore::class)->setAutowired(true);
+        $container->compile();
+
+        $sore = $container->get('sore');
+        var_dump($sore->say()); // Sore -> Ore
+    }
+
     function test_Decorate()
     {
         // Ore を Are でデコレーション
@@ -49,5 +64,26 @@ class SymfonyDependencyInjectionTest extends \PHPUnit_Framework_TestCase
         /* @var $are Are */
         $ore = $container->get('ore');
         var_dump($ore->say()); // Are -> Ore
+    }
+
+    /**
+     * 実行時にインスタンスを設定する
+     */
+    function test_synthetic()
+    {
+        $container = new ContainerBuilder();
+
+        // コンパイル時に必要な事があるので Synthetic なサービスとして定義
+        $container->register('ore', Ore::class)->setSynthetic(true);
+        $container->compile();
+
+        // 実行時にインスタンスを設定
+        $ore = new Ore();
+        $container->set('ore', $ore);
+
+        $obj = $container->get('ore');
+        var_dump($obj->say()); // Ore
+
+        var_dump($obj === $ore); // true
     }
 }
